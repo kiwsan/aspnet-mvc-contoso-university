7434b80e47ec5b5440ac9e11afec603ab39b8ea7
ekiwsant <ekiwsant@THP7DDN86HY1>
2019-09-20 11:35:51 +0700

    remove _logger

--- Common/Exceptions/ManagerWebApiExceptions.cs
+++ Common/Exceptions/ManagerWebApiExceptions.cs
@@ -1,29 +1,25 @@
-﻿using log4net;
-using System;
-using System.IO;
-using System.Reflection;
-using System.Web.Http.ExceptionHandling;
+﻿using System.Web.Http.ExceptionHandling;
 
 namespace Common.Exceptions
 {
     public class ManagerWebApiExceptions : ExceptionLogger
     {
-        ILog _logger = null;
+        //ILog _logger = null;
         public ManagerWebApiExceptions()
         {
-            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(System.Web.HttpContext.Current.Server.MapPath("~/log4net.config")));
+            //log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(System.Web.HttpContext.Current.Server.MapPath("~/log4net.config")));
         }
 
         public override void Log(ExceptionLoggerContext context)
         {
-            _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-            _logger.Error(context.Exception.ToString() + Environment.NewLine);
+            //_logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            //_logger.Error(context.Exception.ToString() + Environment.NewLine);
         }
 
         public void Log(string ex)
         {
-            _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-            _logger.Error(ex);
+            //_logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            // _logger.Error(ex);
         }
 
     }
